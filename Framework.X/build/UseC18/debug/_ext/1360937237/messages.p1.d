0128d78a9e3f80e624ec18405c8d67875451010b
taylorm7 <taylorm7@vt.edu>
2014-02-06 17:44:28 -0500

    first commit, working ADC

---
+++ Framework.X/build/UseC18/debug/_ext/1360937237/messages.p1.d
@@ -0,0 +1,6 @@
+build/UseC18/debug/_ext/1360937237/messages.d  \
+ build/UseC18/debug/_ext/1360937237/messages.p1:  \
+ ../src/messages.c  \
+../src/maindefs.h  \
+../src/messages.h  \
+../src/interrupts.h 
---
+++ Framework.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# UseXC8 configuration
+CND_ARTIFACT_DIR_UseXC8=dist/UseXC8/production
+CND_ARTIFACT_NAME_UseXC8=Framework.X.production.hex
+CND_ARTIFACT_PATH_UseXC8=dist/UseXC8/production/Framework.X.production.hex
+CND_PACKAGE_DIR_UseXC8=${CND_DISTDIR}/UseXC8/package
+CND_PACKAGE_NAME_UseXC8=framework.x.tar
+CND_PACKAGE_PATH_UseXC8=${CND_DISTDIR}/UseXC8/package/framework.x.tar
